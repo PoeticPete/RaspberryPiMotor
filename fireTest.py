3d0b00463c284a150b7f031e8a9816842c9ec72e
PoeticPete <petertao95@gmail.com>
2017-10-28 13:16:07 -0400

    added config

--- fireTest.py
+++ fireTest.py
@@ -1,6 +1,14 @@
 import pyrebase
 
 config = {
-	"apiKey": 
+	"apiKey": "AIzaSyC3tcDJPD4nXPslkhZ7gscE8p9Im4Gw00s",
+	"authDomain": "easy-lock.firebaseapp.com",
+  	"databaseURL": "https://easy-lock.firebaseio.com/",
+  	"storageBucket": "easy-lock.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+storage = firebase.storage()
 
 print("success!")
